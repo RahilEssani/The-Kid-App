febc562f7530950af71eb093e41109c538c48650
Rahil Essani <bitf19a029@pucit.edu.pk>
2022-11-09 02:21:19 +0500

    Feature Added: Random question generation

--- app/src/main/java/com/example/assignment_2_the_kid_app/MainActivity.java
+++ app/src/main/java/com/example/assignment_2_the_kid_app/MainActivity.java
@@ -7,26 +7,59 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     String [] questions= {"Click a box having a unique color.","Click a box having a number", "Click a " +
             "box having red color", "Click a box having a digit 2", "Click a box with the red color having a " +
             "digit 3", "Click a box having a unique digit","Click a box that contains a digit and an alphabet"};
     TextView question,counter,result;
     Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+    Random rnd = new Random();
+    int prev = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         question = findViewById(R.id.question);
         question.setOnClickListener(this);
+        question.setText("Press any button to start...");
     }
 
     @Override
     public void onClick(View view) {
+        System.out.println(view.getId());
         switch (view.getId())
         {
+            case R.id.button:
+                break;
+            case R.id.button10:
+                break;
+            case R.id.button11:
+                break;
+            case R.id.button12:
+                break;
+            case R.id.button13:
+                break;
+            case R.id.button14:
+                break;
+            case R.id.button15:
+                break;
+            case R.id.button16:
+                break;
+            case R.id.button17:
+                break;
             default:
                 break;
         }
+        SetRandomQuestion();
+    }
+    public void SetRandomQuestion(){
+        int index = rnd.nextInt(questions.length);
+        while(index == prev){
+            index = rnd.nextInt(questions.length);
+        }
+        question.setText(questions[index]);
+        prev = index;
     }
 }
