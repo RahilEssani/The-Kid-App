1a902355e99d1c22809a9728569d0f38eeec470e
Rahil Essani <bitf19a029@pucit.edu.pk>
2022-11-09 01:41:34 +0500

    Assigned ids to views

--- app/src/main/java/com/example/assignment_2_the_kid_app/MainActivity.java
+++ app/src/main/java/com/example/assignment_2_the_kid_app/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.assignment_2_the_kid_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView question,counter,result;
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        question = findViewById(R.id.question);
+        question.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            default:
+                break;
+        }
     }
 }
