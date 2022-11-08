f83fee2138d65eade40067ec5eba7d516f31c444
Rahil Essani <bitf19a029@pucit.edu.pk>
2022-11-09 01:47:10 +0500

    Configured click listeners

--- app/src/main/java/com/example/assignment_2_the_kid_app/MainActivity.java
+++ app/src/main/java/com/example/assignment_2_the_kid_app/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    String [] questions= {"Click a box having a unique color.","Click a box having a number", "Click a " +
+            "box having red color", "Click a box having a digit 2", "Click a box with the red color having a " +
+            "digit 3", "Click a box having a unique digit","Click a box that contains a digit and an alphabet"};
     TextView question,counter,result;
     Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
     @Override
